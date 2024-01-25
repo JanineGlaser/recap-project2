abc133c321d5646864ce43d8c4a316e87be45270
Janine Glaser <Jeannyitb.info@gmail.com>
2024-01-25 14:19:02 +0100

    add task1

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // phew… not a lot going on here. Please add some code!
+console.clear;
+
+/*Bookmar-Button*/
+const bookmarkButton = document.querySelector('[data-js="bookmark-button"]');
+
+bookmarkButton.addEventListener("click", () => {
+  bookmarkButton.classList.toggle("bookmark--active");
+});
+
+/*Answer-Button*/
+const answerButton = document.querySelector('[data-js="answer-button"]');
+const showAnswer = document.querySelector('[data-js="show-answer"]');
+
+answerButton.addEventListener("click", () => {
+  showAnswer.classList.toggle("card__answer--active");
+  if (showAnswer.classList.contains("card__answer--active")) {
+    answerButton.textContent = "Hide answer";
+  } else {
+    answerButton.textContent = "Show answer";
+  }
+});
